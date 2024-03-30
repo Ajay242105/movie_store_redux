b094d00dba50b895a0bdc603722411a0ca66b5ae
unknown <ajay766613@gmail.com>
2024-03-31 01:35:08 +0530

    movie-app

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+
 import App from './components/App.jsx';
 import './App.css';
+import movies from './reducers/main.jsx';
+
+const store = createStore(movies);
+console.log('store', store);
+console.log('STATE', store.getState());
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
